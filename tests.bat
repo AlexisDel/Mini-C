e40bed608e7b376cedd99954cd91813cc8df7d72
AlexisDel <github339600@alexisdel.ovh>
2021-12-02 17:12:23 +0100

    Alexis corrige les bêtises de Thomas

--- tests.bat
+++
@@ -1,8 +0,0 @@
-dune build && ./_build/default/minic.exe tests/test_0.mnc 
-./_build/default/minic.exe tests/test.mnc 
-./_build/default/minic.exe tests/test_bool.mnc 
-./_build/default/minic.exe tests/test_fun.mnc 
-./_build/default/minic.exe tests/test_fun_params.mnc 
-./_build/default/minic.exe tests/test_locals.mnc  
-./_build/default/minic.exe tests/test_opp.mnc 
-./_build/default/minic.exe tests/test_limitTest.mnc 
